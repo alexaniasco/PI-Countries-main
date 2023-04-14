6342593984ced21ba036b670da3c7f292624f5ad
alexaniasco <alexaniasco@outlook.com>
2023-04-13 21:43:57 -0300

    UUU

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 require("dotenv").config()
 const {PORT} = process.env
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
